0d950faf74611d63a1fa6d09afef7613429ad820
Rolf Fidelis <hihi313@yahoo.com>
2019-07-20 23:37:34 +0800

    fixing font-family feature

--- FeaturesTree.py
+++ FeaturesTree.py
@@ -2,6 +2,7 @@ from anytree import NodeMixin
 import threading
 from anytree import RenderTree
 from math import log, exp
+import re
 
 class FeaturesTag(NodeMixin):#non-text node
     def __init__(self, name, parent=None, children=None):
@@ -22,11 +23,11 @@ class FeaturesTag(NodeMixin):#non-text node
         self.DOM_derive_features = dict(CNR = None, TD = None, TaD = None, LD = None, CTD = None, DS = None)
         #CSS raw feature
         #show = webelement.is_displayed()
-        self.CSS_features = dict(color = None, lineHeight = None, fontFamily = None, margin = None,
-                                     borderWidth = None, padding = None, width = None, height = None,
-                                     coordinate = None, display = None, visibility = None, show = None)
+        self.CSS_features = dict(color = None, lineHeight = None, fontFamily = None, borderWidth = None,
+                                     margin = None, padding = None, width = None, height = None,
+                                     coordinate = None, display = None)
         #CSS derive features
-        self.CSS_derive_features = dict(area = None, coordinate = None)
+        self.CSS_derive_features = dict(area = None, coordinate = None, show = None)
 class FeaturesText(NodeMixin):#for text node
     def __init__(self, strValue=None, parent=None, children=None):
         super(FeaturesText, self).__init__()
@@ -111,8 +112,57 @@ class FeaturesTree():#DOM tree features sets
         #nLCi = n_char - LCi## of non-link characters under node i         
         #fNode.DOM_derive_features["CTD"] = (n_char/Ti)*log((n_char*Ti/LCi*LTi),log(n_char*LCi/nLCi+LCb*n_char/Cb+exp(1)))
         fNode.DOM_derive_features["DS"] = DS
-    def computeCSSFeatures(self, node, fNode):
-        color = node.value_of_css_property('')
+    def computeCSSFeatures(self, node, fNode):#all length unit in 'px'
+        ###################################common css regex/vars will declare multiple times#############################
+        num_regex = "[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?"
+        #color
+        color = [float(x[0]) for x in re.findall(num_regex, node.value_of_css_property('color'))]
+        #line-height
+        lh_tmp = node.value_of_css_property('line-height')
+        if lh_tmp == "normal":
+            lineHeight = 1.2
+        else:
+            lh_tmp = re.findall(num_regex, lh_tmp)
+            lineHeight = float(lh_tmp[0])
+        #font-family (suffix)
+        ff_tmp = node.value_of_css_property('font-family')
+        fonts = ["serif", "sans-serif", "monospace", "cursive", "fantasy", "system-ui", "emoji", "math", "fangsong"]
+        font_regex = "(serif|sans\-serif|monospace|cursive|fantasy|system\-ui|emoji|math|fangsong)$"
+        fontEnd = re.search(font_regex, ff_tmp).group(0)
+        fontFamily = [1 if f==fontEnd else 0 for f in fonts]
+        #border-width
+        length_re = "px"
+        bw_top = float(re.sub(length_re, "", node.value_of_css_property("border-top")))
+        bw_right = float(re.sub(length_re, "", node.value_of_css_property("border-right")))
+        bw_bottom = float(re.sub(length_re, "", node.value_of_css_property("border-bottom")))
+        bw_left = float(re.sub(length_re, "", node.value_of_css_property("border-left")))
+        borderWidth = [bw_top, bw_right, bw_bottom, bw_left]
+        #margin
+        mg_top = float(re.sub(length_re, "", node.value_of_css_property("margin-top")))
+        mg_right = float(re.sub(length_re, "", node.value_of_css_property("margin-right")))
+        mg_bottom = float(re.sub(length_re, "", node.value_of_css_property("margin-bottom")))
+        mg_left = float(re.sub(length_re, "", node.value_of_css_property("margin-left")))
+        margin = [mg_top, mg_right, mg_bottom, mg_left]
+        #padding
+        pd_top = float(re.sub(length_re, "", node.value_of_css_property("padding-top")))
+        pd_right = float(re.sub(length_re, "", node.value_of_css_property("padding-right")))
+        pd_bottom = float(re.sub(length_re, "", node.value_of_css_property("padding-bottom")))
+        pd_left = float(re.sub(length_re, "", node.value_of_css_property("padding-left")))
+        padding = [pd_top, pd_right, pd_bottom, pd_left]
+        #width, height, area, coordinate (included border & padding)
+        rect = node.rect
+        width = rect.width
+        height = rect.height
+        area = width * height
+        coordinate = [rect.x, rect.y]
+        #display
+        d_tmp = node.value_of_css_property('display')
+        disp = ["inline", "block", "contents", "flex", "grid", "inline-block", "inline-flex", "inline-grid",
+                "inline-table", "list-item", "table", "table-caption", "table-column-group", "table-header-group",
+                "table-footer-group", "table-row-group", "table-cell", "table-column", "table-rownone"]
+        display = [1 if d==d_tmp else 0 for d in disp]
+        #show = webelement.is_displaed()
+        show = 1 if node.is_displayed() else 0
     def printTree(self, root):
         for pre, _, node in RenderTree(root):
             treestr = u"%s%s" % (pre, node.name)
--- selenium_test2.py
+++ selenium_test2.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 from selenium import webdriver
 import FeaturesTree as ft
 from anytree.exporter import JsonExporter
+import re
 
 if __name__ == '__main__':
     path = "D:/Downloads/dragnet_data-master/HTML"
@@ -25,26 +26,30 @@ if __name__ == '__main__':
     print(driver.session_id)
     driver.get("file:///D:/Downloads/dragnet_data-master/HTML/test.html")
     print(driver.current_url)
-    node = driver.find_element_by_css_selector("body > h1")
+    node = driver.find_element_by_css_selector("body > h2")
     print(node.tag_name, 'color:', node.value_of_css_property('color'))
+    num_regex = "[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?"
+    print([float(x[0]) for x in re.findall(num_regex, node.value_of_css_property('color'))])
     print(node.tag_name, 'line-height:', node.value_of_css_property('line-height'))
     print(node.tag_name, 'font-family:', node.value_of_css_property('font-family'))
-    print(node.tag_name, 'margin-top:', node.value_of_css_property('margin-top'))
-    print(node.tag_name, 'margin-right:', node.value_of_css_property('margin-right'))
-    print(node.tag_name, 'margin-bottom:', node.value_of_css_property('margin-bottom'))
-    print(node.tag_name, 'margin-left:', node.value_of_css_property('margin-left'))
-    print(node.tag_name, 'padding-top:', node.value_of_css_property('padding-top'))
-    print(node.tag_name, 'padding-right:', node.value_of_css_property('padding-right'))
-    print(node.tag_name, 'padding-bottom:', node.value_of_css_property('padding-bottom'))
-    print(node.tag_name, 'padding-left:', node.value_of_css_property('padding-left'))
-    print(node.tag_name, 'border-top-width:', node.value_of_css_property('border-top-width'))
-    print(node.tag_name, 'border-right-width:', node.value_of_css_property('border-right-width'))
-    print(node.tag_name, 'border-bottom-width:', node.value_of_css_property('border-bottom-width'))
-    print(node.tag_name, 'border-left-width:', node.value_of_css_property('border-left-width'))
+    arr = ["serif", "sans-serif", "monospace", "cursive", "fantasy", "system-ui", "emoji", "math", "fangsong"]
+    font_regex = "(serif|sans\-serif|monospace|cursive|fantasy|system\-ui|emoji|math|fangsong)$"
+    tmp = re.search(font_regex, node.value_of_css_property("font-family")).group(0)
+    print([1 if x==tmp else 0 for x in arr])
+    print(node.tag_name, 'margin:', node.value_of_css_property('margin'))
+    print(node.tag_name, 'padding:', node.value_of_css_property('padding'))
+    print(node.tag_name, 'border-width:', node.value_of_css_property('border-width'))
     print(node.tag_name, 'rect:', node.rect)
     print(node.tag_name, 'display:', node.value_of_css_property('display'))
+    d_tmp = node.value_of_css_property('display')
+    disp = ["inline", "block", "contents", "flex", "grid", "inline-block", "inline-flex", "inline-grid",
+            "inline-table", "list-item", "table", "table-caption", "table-column-group", "table-header-group",
+            "table-footer-group", "table-row-group", "table-cell", "table-column", "table-rownone"]
+    display = [1 if d==d_tmp else 0 for d in disp]
+    print(display)
     print(node.tag_name, 'visibility:', node.value_of_css_property('visibility'))
     print(node.tag_name, 'is_displayed:', node.is_displayed())
+    print(1 if node.is_displayed() else 0)
     #ftree = ft.FeaturesTree(driver.find_element_by_tag_name("html"), driver)
     #root = ftree.DFT_driver()
     #ftree.printTree(root)
