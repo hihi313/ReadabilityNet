b32cfcd8272d99fe45dcef39cb94edcb801f2f71
Rolf Fidelis <hihi313@yahoo.com>
2019-07-29 11:38:55 +0800

    code beautify

--- selenium_test2.py
+++ selenium_test2.py
@@ -3,39 +3,43 @@ from os.path import isfile, join
 from selenium import webdriver
 import FeaturesTree as ft
 from anytree.exporter import JsonExporter
-import re, time
+import datetime
 
 if __name__ == '__main__':
-    path = "D:/Downloads/dragnet_data-master/HTML"
-    files = [f for f in listdir(path) if isfile(join(path, f))]
-    #print(files)
-    #browser options
-    options = webdriver.ChromeOptions()
-    options.add_argument('-headless')
+    #get all webpage
+    #path = "D:/Downloads/dragnet_data-master/HTML"
+    #files = [f for f in listdir(path) if isfile(join(path, f))]
     #read previous session
     with open("D:/Downloads/browserSession.txt", "r") as f:
         executor_url = f.readline()
         session_id = f.readline()        
         f.close()
-    #attach to exist browser    
+    #browser options
+    options = webdriver.ChromeOptions()
+    options.add_argument('-headless')        
     driver = webdriver.Remote(command_executor=executor_url,
                               desired_capabilities={},
                               options = options)
+    #close new opened windows
+    driver.close()
+    #attach to exist browser
     driver.session_id = session_id
     print(driver.command_executor._url)
     print(driver.session_id)
-    str_load = time.time()
+    #get webpage
     driver.get("file:///D:/Downloads/dragnet_data-master/HTML/test.html")
     print(driver.current_url)
-    str_cvrt = time.time()
+    #start parsing
+    str_cvrt = datetime.datetime.now()
     ftree = ft.FeaturesTree(driver.find_element_by_tag_name("html"), driver, 
-                            "D:\\Downloads\\top_100_fonts_lowercase.csv", "./returnChildNodes.js")
+                            "D:\\Downloads\\top_100_fonts_lowercase.csv", 
+                            "./returnChildNodes.js")
     root = ftree.DFT_driver()
+    #print as tree format
     #ftree.printTree(root)
     #print as JSON format
-    end_cvrt = time.time()
+    end_cvrt = datetime.datetime.now()
     exporter = JsonExporter(indent=2)
     print(exporter.export(root))
-    print(time.strftime('%H:%M:%S', time.gmtime(end_cvrt - str_cvrt)))
-    #driver.close()
-    
+    #print duration time
+    print(end_cvrt - str_cvrt)
