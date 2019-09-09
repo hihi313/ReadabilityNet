f10b89c00f9f77206932c548037ef0ee4a140eab
Rolf Fidelis <hihi313@yahoo.com>
2019-09-09 12:21:52 +0800

    finish improving labeler

--- LCSLabeler.py
+++ LCSLabeler.py
@@ -6,19 +6,25 @@ import FeaturesTree as ft
 from os import listdir
 from os.path import isfile, join
 
+class Label():
+    POSITIVE = 1
+    NEGATIVE = 0
+    NEUTRAL = 2
+
 # common used variables
 class LabelerVars():
-    def __init__(self):
+    def __init__(self, debug = False):
         # REGEX
         self.space_re = "\s+"
         self.after_comment_re = "!@#\$%\^&\*\(\)\s+COMMENTS[\s\S]*"
         # threshold 
         self.label_th = 0.9
         self.shortText_th = 120
+        # debug flag
+        self.debug = debug
         
 class LCSLabeler():
-    ############################################################################ put debug into comVars
-    def __init__(self, comVars, root, gold_standard, debug = False):
+    def __init__(self, comVars, root, gold_standard):
         self.comVars = comVars 
         self.root = root        
         # normalize white space
@@ -26,15 +32,15 @@ class LCSLabeler():
         # remove comment
         self.gold_standard = re.sub(self.comVars.after_comment_re, '', tmp_gld)
         self.lock = threading.Lock()
+        self.body = None
         self.textNodes = []
-        self.debug = debug
     
     def label_driver(self):
         self.getTextNodes(self.root)
         self.textNodes =sorted(self.textNodes, key=self.getTextNodeLen, 
                                reverse=True)
         self.labelTextNodes()
-        self.DFT(self.root, None)
+        self.DFT(self.body, None)
     
     def getTextNodes(self, node):
         # text nodes
@@ -42,6 +48,12 @@ class LCSLabeler():
             self.textNodes.append(node)
         # element nodes
         else:
+            # save the body node
+            if node.tagName == "body":
+                self.body = node
+            # skip anything in <head>
+            if node.tagName == "head":
+                return
             threads = []
             for child in node.children:
                 cThread = threading.Thread(target=self.getTextNodes, 
@@ -58,42 +70,29 @@ class LCSLabeler():
         for tNode in self.textNodes:
             n_match = self.compare(tNode)
             '''
-            If it is "long" content, delete it from gold standard in order to prevent 
-            notation error/nois (for short text)
+            If n_match > 0 (have some chars match), delete it from gold standard 
+            in order to prevent notation error/noise (for short text)
             Delete the content if full match
             '''
-            if n_match > self.comVars.shortText_th:
-                self.gold_standard = re.sub(re.escape(tNode.strValue), '', 
+            if n_match > 0:
+                # preserving a space in order to prevent words "stick" together
+                # count = 1, sub the first result
+                self.gold_standard = re.sub(re.escape(tNode.strValue), ' ', 
                                             self.gold_standard, count = 1)
             tNode.n_match = n_match
-            tNode.similarity = n_match/tNode.DOM_features["n_char"]
+            try:
+                tNode.similarity = n_match/tNode.DOM_features["n_char"]                
+            except ZeroDivisionError:
+                tNode.similarity = 0
+            if tNode.similarity >= self.comVars.label_th:
+                tNode.label = 1
+            else:
+                tNode.label = 0
     
+    # label remaining featuresTag node
+    # do DFT only in visible <body> tag
     def DFT(self, node, pCollector):
-        # text node
-        if node.type == ft.Type.FEATURES_TEXT:
-            self.lock.acquire()
-            match = self.compare(node)
-            '''
-            If it is "long" content, delete it from gold standard in order to prevent 
-            notation error/nois (for short text)
-            Delete the content if full match
-            '''
-            '''
-            may substitute boilerplate first then, cause content cannot be compare 
-            '''
-            if match > self.comVars.shortText_th:
-                self.gold_standard = re.sub(re.escape(node.strValue), '', 
-                                            self.gold_standard, count = 1)
-            self.lock.release()
-            # collect for parent
-            pCollector["n_match"] += match
-            # for debug
-            if self.debug:
-                node.n_match = match
-                node.similarity = match/node.DOM_features["n_char"]
-        # element node
-        else:
-            # # matched char
+        if node.type == ft.Type.FEATURES_TAG:
             collector = {"n_match": 0, "n_pos": 0, "n_neg": 0, "n_neu": 0}
             threads = []
             for child in node.children:
@@ -103,34 +102,67 @@ class LCSLabeler():
                 threads.append(cThread)
             for t in threads:
                 t.join()
-            # labeling
+            node.n_match = collector["n_match"]
             try:
-                node.similarity = collector["n_match"]/node.DOM_features["n_char"]
-                if node.similarity >= self.comVars.label_th:
-                    node.label = 1
-                else:
-                    node.label = 0
-            except AttributeError: # for node.type == Type.TAG
-                pass
+                node.similarity = node.n_match/node.DOM_features["n_char"]                
             except ZeroDivisionError:
-                node.similarity = 0
-            # collect for parent
+                node.similarity = 0            
+            # labeling & collect for parent
             try:
                 pCollector["n_match"] += collector["n_match"]
-            except TypeError:# if no pCollector
+                if not (collector["n_neu"] or collector["n_pos"]):
+                    node.label = Label.NEGATIVE
+                    pCollector["n_neg"] += 1
+                elif collector["n_neu"] or collector["n_pos"] and collector["n_neg"]:
+                    node.label = Label.NEUTRAL
+                    pCollector["n_neu"] += 1                
+                else:
+                    node.label = Label.POSITIVE
+                    pCollector["n_pos"] += 1
+            except TypeError:
+                # not collector , just labeling
+                if not (collector["n_neu"] or collector["n_pos"]):
+                    node.label = Label.NEGATIVE
+                elif collector["n_neu"] or collector["n_pos"] and collector["n_neg"]:
+                    node.label = Label.NEUTRAL
+                else:
+                    node.label = Label.POSITIVE
+        elif node.type == ft.Type.FEATURES_TEXT:
+            try:
+                pCollector["n_match"] += node.n_match
+                if node.label:
+                    pCollector["n_pos"] += 1
+                else:
+                    pCollector["n_neg"] += 1
+            except TypeError:
                 pass
-            # for debug
-            if self.debug:
-                node.n_match = collector["n_match"]
+        else:
+            threads = []
+            for child in node.children:
+                cThread = threading.Thread(target=self.DFT, 
+                                           args=(child, None,))
+                cThread.start()
+                threads.append(cThread)
+            for t in threads:
+                t.join()
 
     # compare node's string value only for text node
     def compare(self, node):
+        '''
+        Always normalize before compare, cause there is modification after 
+        comparison
+        '''
+        # normalize space in text node's text
         txt = re.sub(self.comVars.space_re, ' ', node.strValue)
+        # normalize space in gold standard
+        self.gold_standard = re.sub(self.comVars.space_re, ' ', 
+                                    self.gold_standard)
         # for short string
         if len(txt) <= self.comVars.shortText_th:
             # full match
             if re.search(re.escape(txt), self.gold_standard) != None: #match
-                match = len(node.strValue)#the length of matched string
+                # the length of matched string
+                match = len(node.strValue)
             else:
                 match = 0
         # for long string
@@ -154,7 +186,7 @@ class LCSLabeler():
 
 ################################################################################ normalize features
 
-def labelAPage(comVars, json, correctPath, debug):
+def labelAPage(comVars, json, correctPath):
     jsonFileName = re.sub("[\s\S]*[\\/]", '', re.sub("\.[\s\S]*", '', json))
     # open the file
     importer = JsonImporter()
@@ -163,8 +195,8 @@ def labelAPage(comVars, json, correctPath, debug):
                          "r", encoding = "utf-8").read()
     # start labeling
     str_cvrt = datetime.datetime.now()
-    lbler = LCSLabeler(comVars, root, gold_standard, debug=debug)
-    lbler.DFT_driver()
+    lbler = LCSLabeler(comVars, root, gold_standard)
+    lbler.label_driver()
     end_cvrt = datetime.datetime.now()
     # print duration time
     print(jsonFileName, "takes:", end_cvrt - str_cvrt)      
@@ -173,26 +205,24 @@ def labelAPage(comVars, json, correctPath, debug):
     with open("./labeled_JSON/" + jsonFileName + "_labeled.json", "w") as f:
         f.write(exporter.export(root))
         f.close()  
-    if debug:
+    if comVars.debug:
         with open("./labeled_JSON/log/" + jsonFileName + "_labeled.log", "w", 
                   encoding = 'utf8') as f:
             for pre, _, node in RenderTree(root):
+                treestr = u"%s%s" % (pre, getattr(node, "tagName", "STRING"))
                 try:
-                    treestr = u"%s%s" % (pre, getattr(node, "tagName", "STRING"))
-                    f.write(u"%s %s %s %s %s\n" % (treestr.ljust(8), 
-                          getattr(node, "similarity", '-'), 
-                          getattr(node, "n_match", '-'),
-                          getattr(node, "DOM_features")["n_char"], 
-                          getattr(node, "strValue", '')))
-                except AttributeError:
-                    f.write(u"%s %s %s %s %s\n" % (treestr.ljust(8), 
-                            getattr(node, "similarity", '-'), 
-                            getattr(node, "n_match", '-'),
-                            "-", 
-                            getattr(node, "strValue", '')))
+                    f.write(u"%s %s %s %s %s %s\n" % (treestr.ljust(8), 
+                                                      node.label, 
+                                                      node.similarity, 
+                                                      node.n_match, 
+                                                      node.DOM_features["n_char"], 
+                                                      getattr(node, "strValue", '')))
+                except AttributeError: 
+                    f.write(u"%s %s\n" % (treestr.ljust(8), 
+                                          getattr(node, "strValue", '')))
             f.close()
             
-#################################### debug flag
+# debug flag
 debug = True
 correctPath = "D:/Downloads/dragnet_data-master/Corrected/"
 jsonPath = "D:/OneDrive/Code_Backup/eclipse_workspace/selenium_test2/src/JSON/"
@@ -205,7 +235,7 @@ for f in listdir(jsonPath):
 # sort by size
 jsons = sorted(jsons, key=os.path.getsize)
 # initialize & get common used variables
-com = LabelerVars()
+com = LabelerVars(debug=debug)
 
 jsons = ["D:/OneDrive/Code_Backup/eclipse_workspace/selenium_test2/src/JSON/R249.json"]
 
@@ -218,14 +248,14 @@ while(jsons[start:end]):
         # check whether the file has been processed
         labeledJSON = exportPath + re.sub("[\s\S]*[\\/]", '', 
                                           re.sub("\.[\s\S]*", '', j)) + "_labeled.json"
-        if not os.path.exists(labeledJSON):
-            print("processing:", j)
-            thread = threading.Thread(target=labelAPage, 
-                                      args=(com, j, correctPath, debug))
-            thread.start()
-            threads.append(thread)
-        else:
-            print("skip:", j)
+        #if not os.path.exists(labeledJSON):
+        print("processing:", j)
+        thread = threading.Thread(target=labelAPage, 
+                                  args=(com, j, correctPath))
+        thread.start()
+        threads.append(thread)
+        #else:
+        #    print("skip:", j)
     for t in threads:
         t.join()
     start = end
