bc147f61a23f0bbddf3c18790e9dbce46fb2eee5
Rolf Fidelis <hihi313@yahoo.com>
2019-10-28 19:50:51 +0800

    add file name in debug message

--- inputToNN.py
+++ inputToNN.py
@@ -1,13 +1,12 @@
 import numpy as np, os, threading, FeaturesTree as ft
 from anytree.importer import JsonImporter
 from collections import OrderedDict
-from cProfile import label
 
 debug = True
 printMutex = threading.Lock()
 dataMutex = threading.Lock()
 
-def extract(node, xTrain, yTrain):
+def extract(node, xTrain, yTrain, fName):
     # take only FEATURES_TAG as NN input
     if node.type == ft.Type.FEATURES_TAG:
         features = []
@@ -24,7 +23,7 @@ def extract(node, xTrain, yTrain):
                 features = features + [f]
                 if debug:
                     printMutex.acquire()
-                    print("f:%s, ERROR:%s" % (f, err))
+                    print("file:%s, f:%s, ERROR:%s" % (fName, f, err))
                     printMutex.release()
         # CSS derive features
         features = features + list(node.CSS_derive_features.values())
@@ -36,7 +35,8 @@ def extract(node, xTrain, yTrain):
     # traverse downward        
     threads = []
     for child in node.children:
-        cThread = threading.Thread(target=extract, args=(child, xTrain, yTrain,))
+        cThread = threading.Thread(target=extract, args=(child, xTrain, yTrain, 
+                                                         fName))
         cThread.start()
         threads.append(cThread)
     for t in threads:
@@ -60,11 +60,14 @@ threads = []
 for f in files:
     importer = JsonImporter(object_pairs_hook = OrderedDict)
     root = importer.read(open(f, encoding="utf-8"))
-    thread = threading.Thread(target=extract, args=(root, xTrain, yTrain,))
+    print("processing:", f)
+    thread = threading.Thread(target=extract, args=(root, xTrain, yTrain, f,))
     thread.start()
     threads.append(thread)
 for t in threads:
     t.join()
+    
+print("done")    
     
 xTrain = np.array(xTrain)
 yTrain = np.array(yTrain)
