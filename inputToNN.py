2a158711dfdf5a1a7b3ef9d5086ed22466e888b4
Rolf Fidelis <hihi313@yahoo.com>
2019-11-01 10:05:08 +0800

    splitting .npy finished, debugging data length inconsistent in FeaturesTree.py

--- inputToNN.py
+++ inputToNN.py
@@ -2,81 +2,20 @@ import numpy as np, os, threading, FeaturesTree as ft
 from anytree.importer import JsonImporter
 from collections import OrderedDict
 
-class CustomError(Exception):
+class NoneTypeError(Exception):
     pass
 
-debug = False
+debug = True
 printMutex = threading.Lock()
 dataMutex = threading.Lock()
-fileIndex = 1
 
+features = []
+labels = []
 xTrain = []
 yTrain = []
 
-def append(features, node, fName = None):
-    global fileIndex, xTrain, yTrain
-    if len(xTrain) >= 1000:
-        # store every 1000 data to file
-        print(np.array(xTrain).shape, "File:%s, parent:%s, node:%s" % 
-              (fName, node.parent.tagName, node.tagName))
-        np.save((labeledPath + "xTrain%d.npy") % (fileIndex), 
-                np.array(xTrain))
-        np.save((labeledPath + "yTrain%d.npy") % (fileIndex), 
-                np.array(yTrain))
-        fileIndex = fileIndex + 1
-        xTrain = []
-        yTrain = []
-        print("xTrain", xTrain)
-    
-    xTrain.append(features)
-    yTrain.append(node.label)#yTrain.append(node.similarity)
-    
-    if debug:
-        with printMutex:
-            try:
-                print("File:%s, parent:%s, node:%s" % (fName, node.parent.tagName, node.tagName))
-                print("len:%d" % (len(xTrain)))
-                for i in xTrain:
-                    print(len(i))        
-                print("shape:", np.array(xTrain).shape)
-                print("shapeY:", np.array(yTrain).shape)
-            except BaseException:
-                pass
-
 def extract(node, fName):
-    global dataMutex, printMutex
-    # take only FEATURES_TAG as NN input
-    if node.type == ft.Type.FEATURES_TAG:
-        try:
-            features = []
-            # DOM features
-            features = features + list(node.DOM_features.values())
-            # DOM derive features
-            features = features + list(node.DOM_derive_features.values())
-            # CSS features
-            # fontFamily length inconsist cross file, delete it
-            del node.CSS_features["fontFamily"]
-            CSSFeatures = node.CSS_features.values()
-            for f in CSSFeatures:
-                try:
-                    features = features + f
-                except TypeError as err:
-                    features = features + [f]
-                    if f == None:
-                        raise CustomError("None type accure")
-                    if debug:
-                        with printMutex:
-                            print("file:%s, f:%s, ERROR:%s" % (fName, f, err))
-            # CSS derive features
-            features = features + list(node.CSS_derive_features.values())
-            # append to input data arrays
-            with dataMutex:
-                append(features, node, fName)
-        except CustomError as err:
-            # ignore None data
-            with printMutex:
-                print("ERROR:%s, file:%s, parent:%s, node:%s" % 
-                      (err, fName, node.parent, node))
+    global dataMutex, printMutex, features, labels
     # traverse downward        
     threads = []
     for child in node.children:
@@ -85,15 +24,45 @@ def extract(node, fName):
         threads.append(cThread)
     for t in threads:
         t.join()
-        
-    try:
-        if node.tagName == "html":
-            with printMutex:
-                print("file:%s finished" % (fName))
-    except AttributeError:
-        pass
+    # take only FEATURES_TAG as NN input
+    if node.type == ft.Type.FEATURES_TAG:
+        try:
+            nodeFeatures = []
+            # DOM features
+            nodeFeatures = nodeFeatures + list(node.DOM_features.values())
+            # DOM derive features
+            nodeFeatures = nodeFeatures + list(node.DOM_derive_features.values())
+            # CSS features
+            # fontFamily length inconsist cross file, delete it
+            del node.CSS_features["fontFamily"]
+            CSSFeatures = node.CSS_features.values()
+            for f in CSSFeatures:
+                try:
+                    nodeFeatures = nodeFeatures + f
+                except TypeError as err:
+                    nodeFeatures = nodeFeatures + [f]
+                    if f == None:
+                        raise NoneTypeError("None type occure")
+            # CSS derive features
+            nodeFeatures = nodeFeatures + list(node.CSS_derive_features.values())
+            # append to input data arrays
+            with dataMutex:
+                features.append(nodeFeatures)
+                labels.append(node.label)
+                if debug:
+                    print(features.index(nodeFeatures), 
+                          "parent:%s, node:%s %s" % 
+                          (node.parent.tagName, node.tagName, node))
+        except NoneTypeError as err:
+            # ignore None data
+            if debug:
+                with printMutex:
+                    print("ERROR:%s, file:%s, parent:%s, node:%s" % 
+                          (err, fName, node.parent, node))
         
 labeledPath = "D:/Downloads/labeled_JSON/"
+fileIndex = 1
+N = 100000
 
 files = []
 for f in os.listdir(labeledPath):
@@ -102,25 +71,43 @@ for f in os.listdir(labeledPath):
         files.append(p)
 # sort by size
 files = sorted(files, key=os.path.getsize)
-#files = files[0:4]#["D:/Downloads/labeled_JSON/R427_labeled.json"]
+files = files[19:20]#["D:/Downloads/labeled_JSON/R427_labeled.json"]
 
 
 threads = []
 for f in files:
     importer = JsonImporter(object_pairs_hook = OrderedDict)
     root = importer.read(open(f, encoding="utf-8"))
-    #print("processing:", f)
-    thread = threading.Thread(target=extract, args=(root, f,))
-    thread.start()
-    threads.append(thread)
-for t in threads:
-    t.join()
+    print(files.index(f), "processing:", f)
+    extract(root, f)    
+    xTrain = xTrain + features
+    yTrain = yTrain + labels
+    if debug:
+        print("features.shape:", np.array(features).shape)
+        print("xTrain.shape:", np.array(xTrain).shape)
+        print("yTrain.shape:", np.array(yTrain).shape)
+        #'''
+        for i in range(len(features)):
+            print("len(feature[%d]):" % (i), len(features[i]))
+        for i in range(len(xTrain)):
+            print("len(xTrain[%d]):" % (i), len(xTrain[i]))
+        try:
+            for i in range(len(yTrain)):
+                print("len(yTrain[%d]):" % (i), len(yTrain[i]))
+        except TypeError:
+            pass
+        #'''
+    features = []
+    if len(xTrain) > N:
+        np.save(labeledPath + "xTrain%d.npy" % (fileIndex), np.array(xTrain))
+        np.save(labeledPath + "yTrain%d.npy" % (fileIndex), np.array(yTrain))
+        fileIndex = fileIndex + 1
+        xTrain = []
+        yTrain = []
+    print("finished:", f)
     
 print("done")    
     
 # store the remaining data
-xTrain = np.array(xTrain)
-yTrain = np.array(yTrain)
-
-np.save(labeledPath + "xTrain.npy", xTrain)
-np.save(labeledPath + "yTrain.npy", yTrain)
+np.save(labeledPath + "xTrain%d.npy" % (fileIndex), np.array(xTrain))
+np.save(labeledPath + "yTrain%d.npy" % (fileIndex), np.array(yTrain))
